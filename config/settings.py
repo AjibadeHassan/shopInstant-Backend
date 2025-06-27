0ab05882c8d589c09199659465fad876aa0021e4
Ajibade Hassan <hassanajibade17@gmail.com>
2025-06-27 01:55:02 +0100

    shy of api

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'djoser',
+    'store',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +125,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # Change to IsAuthenticated in prod
+    ],
+}
+
--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+
+# class ProductCategory(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='categories')
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+#     class Meta:
+#         unique_together = ('product', 'category')
+
+#     def __str__(self):
+#         return f"{self.product.name} - {self.category.name}"
